35aada529ccd46ffbd5d23176f9b438207338fe0
Dr. Renju Mathew <renju.s.mathew@gmail.com>
2021-12-27 12:25:36 +0000

    Web app completed

--- app/run.py
+++ app/run.py
@@ -8,7 +8,8 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+# from sklearn.externals import joblib (Note that this is deprecated)
+import joblib
 from sqlalchemy import create_engine
 
 
@@ -26,11 +27,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('DisasterResponse', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -38,19 +39,38 @@ model = joblib.load("../models/your_model_name.pkl")
 @app.route('/index')
 def index():
     
-    # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
-    genre_names = list(genre_counts.index)
+    # Data needed for visuals
     
+    # Data for graph 1
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index.str.replace("_", " ").str.title())
+    print(genre_counts)
+    print(genre_names)
+    
+    # Data for graph 2
+    categories = [column for column in df.columns if column not in ['index', 'id', 'message', 'original', 'genre']]
+    category_df = df[categories].sum() 
+    category_counts = list(category_df.values)
+    category_names = list(category_df.index.str.replace("_", " ").str.title())
+    print(category_counts)
+    print(category_names)
+    
+    # Data for graph 3
+    category_df = pd.DataFrame(category_df).rename(columns = {0: "counts"})
+    top5 = category_df.sort_values(by = 'counts', ascending = False).head(6)[1:6]
+    top5_names = list(top5.index.str.replace("_", " ").str.title())
+    top5_values = list(top5.counts.values)
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
+    # Graph 1
     graphs = [
         {
             'data': [
                 Bar(
                     x=genre_names,
-                    y=genre_counts
+                    y=genre_counts,
+                    width=.5,
+                    textfont = {'family' : 'Times New Roman'},
+                        marker=dict(color='BlueViolet')
                 )
             ],
 
@@ -61,11 +81,71 @@ def index():
                 },
                 'xaxis': {
                     'title': "Genre"
-                }
-            }
+
+                },
+                'padding': 150
+                
+            },
+
         }
     ]
     
+    # Graph 2
+    graphs.append({
+    'data': [
+        Bar(
+            x=category_names,
+            y=category_counts,
+            orientation = 'v',
+            width=.5,
+            textfont = {'family' : 'Times New Roman'},
+                marker=dict(color='navy')
+        )
+    ],
+
+    'layout': {
+        'title': 'Count of Messages in Different Categories',
+        'yaxis': {
+            'title': "Count"
+        },
+        'xaxis': {
+            'title': "Category",
+            'automargin': True
+        },
+                'padding': 150
+    },
+
+    })
+
+    # Graph 3
+    graphs.append({
+            'data': [
+                Bar(
+                    y=top5_values,
+                    x=top5_names,
+                    orientation = 'v',
+                    width=.5,
+                    textfont = {'family' : 'Arial'},
+                        marker=dict(color='Indigo')
+                )
+            ],
+
+            'layout': {
+                'title': 'Top Five Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                },
+                'padding': 1000
+            }
+
+
+        })
+        
+
+        
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
@@ -93,8 +173,9 @@ def go():
 
 
 def main():
+    # Note: Use http://localhost:3001
     app.run(host='0.0.0.0', port=3001, debug=True)
-
+    
 
 if __name__ == '__main__':
     main()
